926237ae896dd876f36550509d963b46bd239910
JoshX <joshua.osagie01@gmail.com>
2021-08-01 00:47:59 +0100

    feat(navbar) mobile responsiveness
    Add username search component

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import './assets/tailwind.css';
 import router from './router';
-import { createProvider } from './vue-apollo'
+// import { createProvider } from './vue-apollo'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  apolloProvider: createProvider(),
+  // apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount('#app')
