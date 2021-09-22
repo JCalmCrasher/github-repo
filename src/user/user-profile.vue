0a9e143bd18a09cdb78530bd517bd17eced5f2bf
JoshX <joshua.osagie01@gmail.com>
2021-09-22 01:39:12 +0100

    :fire: :rocket: env variables

--- src/user/user-profile.vue
+++ src/user/user-profile.vue
@@ -135,12 +135,6 @@ export default {
     Card,
     CardPaginate,
   },
-  data() {
-    return {
-      userBio:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
-    };
-  },
 };
 </script>
 
